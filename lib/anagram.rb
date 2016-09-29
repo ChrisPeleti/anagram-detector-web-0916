9b5387a804c4090c56d23236fccbef687632047b
ChrisPeleti <chrisyred@gmail.com>
2016-09-29 14:54:17 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+  def alphabetical_word
+    @word.split(//).sort!
+  end
+
+  def match(array_words)
+    array_words.select {|scrambled_word| scrambled_word.split(//).sort==alphabetical_word}
+
+  end
+
+
+end
